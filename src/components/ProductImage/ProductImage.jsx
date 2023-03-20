2a934a16bdda96cc7dae91c94fe45c68ec92331d
RopeJuve <rope.s@hotmail.com>
2023-03-20 10:38:01 +0100

    Product image

--- src/components/ProductImage/ProductImage.jsx
+++ src/components/ProductImage/ProductImage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import "./ProductImage.css";
+import img from "../../assets/images/image-product-1.jpg";
+import prev from "../../assets/images/icon-previous.svg";
+import next from "../../assets/images/icon-next.svg";
 
 const ProductImage = () => {
   return (
-    <div>ProductImage</div>
-  )
-}
+    <div className="productImage__container">
+      <img src={img} alt="productImage" />
+      <div className="arrows">
+        <button className="arrow">
+          <img src={prev} alt="arrowPrev"></img>
+        </button>
+        <button className="arrow">
+          <img src={next} alt="arrowNext"></img>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProductImage
+export default ProductImage;
