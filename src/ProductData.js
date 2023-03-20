92508af2b30654d8cd6b89f563e7d39ec65d9814
RopeJuve <rope.s@hotmail.com>
2023-03-20 13:24:48 +0100

    Product details

---
+++ src/ProductData.js
@@ -0,0 +1,7 @@
+export const ProductData = {
+  subtitle: "Sneaker Company",
+  title: "Fall Limited Edition Sneakers",
+  description:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outlet sole, they'll withstand everything the weather can offer.",
+  price: 125.00,
+};
--- src/components/ProductDescription/ProductDescription.jsx
+++ src/components/ProductDescription/ProductDescription.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import "./ProductDescription.css";
+import cart from "../../assets/images/icon-cart.svg";
+import { ProductData } from "../../ProductData";
+import { useState } from "react";
 
 const ProductDescription = () => {
-  return (
-    <div>ProductDescription</div>
-  )
-}
+  const [cartItem, setCartItem] = useState(0);
 
-export default ProductDescription
+  const { subtitle, title, description, price } = ProductData;
+  return (
+    <div className="productDescription__container">
+      <div className="description-wrapper">
+        <h3>{subtitle}</h3>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <div className="price-container">
+          <div className="price-wrapper">
+            <strong>${price}.00</strong>
+            <p>50%</p>
+          </div>
+          <strong className="price-sale">$250.00</strong>
+        </div>
+        <div className="buttons">
+          <input type="number" value={cartItem}></input>
+          <img src={cart}></img>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDescription;
